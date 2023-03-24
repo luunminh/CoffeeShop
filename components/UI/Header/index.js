83d06d868a4eeb0629c642c0e757631ac1385cb3
Nguyen Ngoc Phu <phunguyen291002@gmail.com>
2023-03-24 11:19:41 +0700

    Resolve merge conflict

--- components/UI/Header/index.js
+++ components/UI/Header/index.js
@@ -2,10 +2,23 @@ import React from 'react'
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
 import UserAvatar from 'react-native-user-avatar-component'
 import Colors from '../../Colors'
-export default function Header({ navigator, user }) {
+import { Toast } from 'react-native-toast-message/lib/src/Toast'
+function Header({ navigation, user, reloadFunc }) {
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.leftSide}>
+            <TouchableOpacity style={styles.leftSide}
+                onPress={() => {
+                    reloadFunc(prev => !prev)
+                    Toast.show({
+                        type: 'success',
+                        text1: "Reload your data",
+                        // text2: "There are some errors while processing !!!",
+                        autoHide: 'true',
+                        visibilityTime: 1000
+
+                    })
+                }}
+            >
                 <Image
                     style={styles.nameLogo}
                     source={require('../../../assets/img/nameLogo.png')}
@@ -14,9 +27,11 @@ export default function Header({ navigator, user }) {
             <TouchableOpacity style={styles.rightSide}>
                 <UserAvatar size="48" color={Colors.textColor} name={`${(user) ? user.photoURL : 'Son Hoang'}`} src={`${(user) ? user.photoURL : ''}`} />
             </TouchableOpacity>
+            <Toast />
         </View>
     )
 }
+export default React.memo(Header)
 
 const styles = StyleSheet.create({
     container: {
